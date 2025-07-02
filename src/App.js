b1e8865c50643e4ae4a9d3b0b23942062574f9ba
ingreyeslara <ingreyeslara@gmail.com>
2025-07-02 13:21:50 -0600

    commit examen parcial 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './alumno.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          JUAN PÉREZ SOSA  - EXAMEN PARCIAL 2
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https:www.utd.edu.mx"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          UNIVERSIDAD TECNOLÓGICA DE DURANGO - CLICK PARA VISITAR EL SITIO
         </a>
       </header>
     </div>
