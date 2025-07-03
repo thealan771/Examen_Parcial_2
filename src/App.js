e242cf091feb3597c037541283540125fbb8496e
thealan771 <gurrolaalan771@gmail.com>
2025-07-03 15:23:38 -0600

    Subiendo proyecto para Examen Parcial 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          JUAN PÉREZ SOSA  - EXAMEN PARCIAL 2
+          Gurrola Trujillo ALan  - EXAMEN PARCIAL 2
         </p>
         <a
           className="App-link"
